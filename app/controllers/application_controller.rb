01e3acaf82b1035de0f38338d2e7eeed9e0823c8
EC2 Default User <ec2-user@ip-172-31-46-87.ap-southeast-2.compute.internal>
2022-05-01 14:10:52 +0000

    [Add]adminログイン機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def after_sign_in_path_for(resource)
-	  root_path
-  end
+  #def after_sign_in_path_for(resource)
+	  #customers_show_path
+  #end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code ,:address, :telephone_number])
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,11 @@ devise_for :customers,skip: [:passwords], controllers: {
   sessions: 'public/sessions'
 }
 root to: 'public/homes#top'
-get "/home/about" => "public/homes#about", as: "about"
+get "/home/about" => "public/homes#about"
 get '/items' => 'public/items#index'
+get "/customers/show" => "public/customers#show"
+
+get "/admin" => "admin/homes#top"
 # URL /admin/sign_in ...
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
