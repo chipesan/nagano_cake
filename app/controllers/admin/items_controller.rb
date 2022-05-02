12e08b9508d1fbe09722d52b05fd5e16c75ae012
EC2 Default User <ec2-user@ip-172-31-46-87.ap-southeast-2.compute.internal>
2022-05-02 16:29:35 +0000

    [Add]商品新規登録機能の実装途中

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,32 @@
 class Admin::ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+
+  def new
+    @items = Item.new(genre_params)
+  end
+
+  def create
+    @items = Item.new(genre_params)
+    if @items.save
+      # 登録できた時
+      redirect_to admin_items_index_path
+    else
+      # 登録できなかった時
+      @items = Item.all
+      render :index
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ post "admin/genres" => "admin/genres#create", as: "admin_genres_create"
 get "admin/items" => "admin/items#index", as: "admin_items_index"
 get "admin/customers" => "admin/customers#index", as: "admin_customers_index"
 patch "/admin/genres/:id" => "admin/genres#update", as: "admin_genres_update"
+get "admin/items/new" => "admin_items#new", as: "admin_items_new"
+post "admin/items" => "admin/items#create", as: "admin_items_create"
 # URL /admin/sign_in ...
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
