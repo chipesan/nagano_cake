257cc8bf5d72328ea184317677ad7c5486f0a77f
EC2 Default User <ec2-user@ip-172-31-46-87.ap-southeast-2.compute.internal>
2022-05-02 11:53:04 +0000

    ヘッダー条件分岐作成

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,12 @@ devise_for :customers,skip: [:passwords], controllers: {
 }
 root to: 'public/homes#top'
 get "/home/about" => "public/homes#about"
-get '/items' => 'public/items#index'
-get "/customers/show" => "public/customers#show"
+get '/items' => 'public/items#index', as: "public_items_index"
+get "/customers/show" => "public/customers#show", as: "public_customers_show"
+get "/cart_items" => "public/cart_items#index", as: "public_cart_items_index"
 
 get "/admin" => "admin/homes#top"
-get "/admin/genres" => "admin/genres#index"
+get "/admin/genres" => "admin/genres#index", as: "admin_genres_index"
 get "/admin/genres/:id/edit" => "admin/genres#edit", as: "admin_genres_edit"
 post "admin/genres" => "admin/genres#create", as: "admin_genres_create"
 get "admin/items" => "admin/items#index", as: "admin_items_index"
