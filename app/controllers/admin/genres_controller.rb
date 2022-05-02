c0ecc5599e6bec6fbf579422a606afa9d9816a45
EC2 Default User <ec2-user@ip-172-31-46-87.ap-southeast-2.compute.internal>
2022-05-02 09:07:10 +0000

    [Add]編集ボタン作成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,32 @@
 class Admin::GenresController < ApplicationController
+  def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      # 登録できた時
+      redirect_to admin_genres_path
+    else
+      # 登録できなかった時
+      @genres = Genre.all
+      render :index
+    end
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+  end
+
+  private
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ get '/items' => 'public/items#index'
 get "/customers/show" => "public/customers#show"
 
 get "/admin" => "admin/homes#top"
+get "/admin/genres" => "admin/genres#index"
+get "/admin/genres/:id/edit" => "admin/genres#edit", as: "admin_genres_edit"
+post "admin/genres" => "admin/genres#create", as: "admin_genres_create"
 # URL /admin/sign_in ...
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
