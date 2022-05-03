efde7391e0b088c0bc92d661c2dec7fe3b69ee17
EC2 Default User <ec2-user@ip-172-31-46-87.ap-southeast-2.compute.internal>
2022-05-03 22:12:22 +0000

    [Add]会員一覧画面の実装途中

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,4 +1,22 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Item.all
   end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+  end
+
+  private
+  def Customer_params
+    params.require(:customer).permit(:name, :email, :encrypted_password, :postal_code, :address, :phonenumber, :is_active)
+  end
+
 end
