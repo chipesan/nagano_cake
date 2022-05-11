6bf9feac8d5b34350d8f4135bca6e390125787f2
EC2 Default User <ec2-user@ip-172-31-46-87.ap-southeast-2.compute.internal>
2022-05-11 08:11:58 +0000

    [Add]商品一覧画面の実装途中

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,5 +1,11 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.all  
+    @items = Item.all
+    @genres = Genre.all
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :price, :image)
   end
 end
